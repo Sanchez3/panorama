9e0714ead4d98ab54727c5f0f3f96e3c0540ebcb
硫酸 <lsch001@163.com>
2018-05-21 17:01:47 +0800

    :sparkles: CSS3DRenderer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,13 +15,86 @@ import css from '../css/css.css';
 // import Js Plugins/Entities
 
 // import THREE from 'three';
-
+import CSS3DRenderer from 'three/examples/js/renderers/CSS3DRenderer.js';
 import CanvasRenderer from 'three/examples/js/renderers/CanvasRenderer.js';
 import DeviceOrientationControls from 'three/examples/js/controls/DeviceOrientationControls.js';
 import Projector from 'three/examples/js/renderers/Projector.js';
 
 window.h5 = {
-    initThree: function() {
+    initCss3DR: function() {
+        var camera, scene, renderer;
+        var geometry, material, mesh;
+        var controls;
+        init();
+        animate();
+
+        function init() {
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+            controls = new THREE.DeviceOrientationControls(camera);
+            scene = new THREE.Scene();
+            var sides = [{
+                    url: './assets/img/px.jpg',
+                    position: [-512, 0, 0],
+                    rotation: [0, Math.PI / 2, 0]
+                },
+                {
+                    url: './assets/img/nx.jpg',
+                    position: [512, 0, 0],
+                    rotation: [0, -Math.PI / 2, 0]
+                },
+                {
+                    url: './assets/img/py.jpg',
+                    position: [0, 512, 0],
+                    rotation: [Math.PI / 2, 0, Math.PI]
+                },
+                {
+                    url: './assets/img/ny.jpg',
+                    position: [0, -512, 0],
+                    rotation: [-Math.PI / 2, 0, Math.PI]
+                },
+                {
+                    url: './assets/img/pz.jpg',
+                    position: [0, 0, 512],
+                    rotation: [0, Math.PI, 0]
+                },
+                {
+                    url: './assets/img/nz.jpg',
+                    position: [0, 0, -512],
+                    rotation: [0, 0, 0]
+                }
+            ];
+            var cube = new THREE.Object3D();
+            scene.add(cube);
+            for (var i = 0; i < sides.length; i++) {
+                var side = sides[i];
+                var element = document.createElement('img');
+                element.width = 1026; // 2 pixels extra to close the gap.
+                element.src = side.url;
+                var object = new THREE.CSS3DObject(element);
+                object.position.fromArray(side.position);
+                object.rotation.fromArray(side.rotation);
+                cube.add(object);
+            }
+            renderer = new THREE.CSS3DRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(renderer.domElement);
+            //
+            window.addEventListener('resize', onWindowResize, false);
+        }
+
+        function onWindowResize() {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        }
+
+        function animate() {
+            requestAnimationFrame(animate);
+            controls.update();
+            renderer.render(scene, camera);
+        }
+    },
+    initWebGLR: function() {
         var camera, scene, renderer;
         var controls;
         var texture_placeholder,
@@ -38,8 +111,6 @@ window.h5 = {
 
         init();
         animate();
-
-
 
         function init() {
             var container, mesh;
@@ -215,7 +286,7 @@ window.h5 = {
     init: function() {
         var that = this;
         that.cssInit().eventInit();
-        that.initThree();
+        that.initCss3DR();
     }
 };
 
